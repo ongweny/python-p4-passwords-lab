80135b41ce2f4ecd6592fb8b1154b5738b165d67
ongweny <ongwenyjoel@gmail.com>
2024-08-04 15:17:01 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -2,7 +2,6 @@
 
 from flask import request, session
 from flask_restful import Resource
-
 from config import app, db, api
 from models import User
 
@@ -28,16 +27,43 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
 
+        if session.get('user_id'):
+            
+            user = User.query.filter(User.id == session['user_id']).first()
+            
+            return user.to_dict(), 200
+
+        return {}, 204
+    
 class Login(Resource):
-    pass
+    def post(self):
 
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        user = User.query.filter(User.username == username).first()
+
+        if user.authenticate(password):
+
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {'error': '401 Unauthorized'}, 401
+    
 class Logout(Resource):
-    pass
+   def delete(self):
 
+        session['user_id'] = None
+
+        return {}, 204
+   
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
